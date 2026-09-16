d810e2bcf5b207fff63011d97c948bd02308e69a
hariram <YOUR_GITHUB_EMAIL@gmail.com>
2026-09-16 21:24:14 +0530

    fix database connection and gitignore

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,19 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const db = mysql.createPool(process.env.DATABASE_URL);
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Test MySQL connection
 db.getConnection()
   .then((connection) => {
     console.log("MySQL cloud connection successful");
